8fbb16a937efb570fb055cd8bf10733a9aea2021
Mike VanSleen <mike.vansleen@gmail.com>
2019-08-31 20:33:42 -0600

    Added users table migration

--- data/migrations/20190831202348_bootstrap.js
+++ data/migrations/20190831202348_bootstrap.js
@@ -1,8 +1,21 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("users", users => {
+    users.increments();
+
+    users
+      .string("username", 255)
+      .notNullable()
+      .unique();
+    users.string("password", 255).notNullable();
+    users
+      .string("email", 255)
+      .notNullable()
+      .unique();
+    users.string("about_me", 4000);
+    users.string("profile_image_url", 4000);
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("users");
 };
