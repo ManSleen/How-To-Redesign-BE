bc61db6ee3cedc68fd6057ee9f002af326905d92
Mike VanSleen <mike.vansleen@gmail.com>
2019-10-05 12:23:19 -0600

    Added token when user registers

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -13,7 +13,8 @@ router.post("/register", (req, res) => {
 
   Users.add(user)
     .then(saved => {
-      res.status(201).json(saved);
+      const token = generateToken(user);
+      res.status(201).json({saved, token});
     })
     .catch(error => {
       res.status(500).json({
